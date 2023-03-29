9df9d6c226af76a3c2c9854137e78cea8ce1aff7
AndreasKueck <ak163@gmx.net>
2023-03-29 12:57:25 +0200

    Update sonochatgpt.php

--- sonochatgpt.php
+++ sonochatgpt.php
@@ -1,5 +1,5 @@
 <?php
 $ch = curl_init("https://script.google.com/macros/s/AKfycbwqVSaIRXB0Pp-ehmv1QCLCzjL59zI7-bo18BBAklJVQPnHNp63TGB-ltE0W3YpSsad/exec");
-curl_setopt($ch, CURLOPT_POST, 1);
+curl_exec($ch);
 echo '<?xml version="1.0" encoding="UTF-8"?><Response><Play>https://docs.google.com/uc?export=play&amp;id=1BJHIONgZAyZcg6AYCRPHAsGervuTm7fZ</Play></Response>';
 ?>
